a64a6619d1056624414f3e157d02717ae0900a97
Yuri-Campos <ygsoares20@gmail.com>
2023-12-12 19:05:46 -0300

    sounds play faster when running

--- Player/player.gd
+++ Player/player.gd
@@ -23,7 +23,8 @@ var obj_cam
 
 var distance_footstep: float = 0.0
 var play_footstep: int = 3
-
+var is_running: bool = false
+var stamina: float = 100.0
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -32,7 +33,10 @@ func _input(event):
 		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
 		camera_3d.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
 		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-89), deg_to_rad(89))
-
+	if Input.is_action_just_pressed("action_run"):
+		is_running = true
+	if Input.is_action_just_released("action_run"):
+		is_running = false
 func _process(delta):
 	if(is_bob_on):
 		cam_movement(delta)	
@@ -70,26 +74,35 @@ func process_movement(delta) -> void:
 	
 	direction = Vector3(direction.x, 0, direction.z).rotated(Vector3.UP, h_rot).normalized()
 	
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+	
+	var char_speed = speed * 1.5 if is_running && stamina > 0 else speed
+	velocity.x = direction.x * char_speed
+	velocity.z = direction.z * char_speed
 	
 	if !is_on_floor():
 		velocity.y -= GRAVITY * delta
 	if Input.is_action_just_pressed("action_jump") and is_on_floor():
 		velocity.y = jump_vel
 	
-
 	move_and_slide()
 	
 func process_ground_sounds(group: String):
-	if play_footstep != 100 and (int(velocity.x) != 0) || int (velocity.z) != 0:
+	
+	if is_running:
+		play_footstep = 2
+	else:
+		play_footstep = 3
+		
+		
+	if (int(velocity.x) != 0) || int (velocity.z) != 0:
 		distance_footstep += 0.1
 		
 	if distance_footstep > play_footstep and is_on_floor():
 		match group:
 			"terrain_wood":
 				step_sound.stream = load("res://sounds/steps/wood/1.ogg")
-				
+			"terrain_grass":
+				step_sound.stream = load("res://sounds/steps/grass/1.ogg")	
 		
 		step_sound.pitch_scale = randf_range(0.8, 1.2)
 		step_sound.play()
