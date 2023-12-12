409a05967fda6c0b0613a88d6a65064751d41a61
Yuri-Campos <ygsoares20@gmail.com>
2023-12-12 03:03:31 -0300

    create a basic headbob system for testing

--- Player/player.gd
+++ Player/player.gd
@@ -2,14 +2,21 @@ extends CharacterBody3D
 
 
 @onready var camera_3d = $Camera3D
+@onready var origin_cam_pos: Vector3 = camera_3d.position
 
 @export var mouse_sens: float= 0.15
+@export var cam_bob_speed: float = 6
+@export var cam_bob_up_down: float = 0.5
 
-const GRAVITY: int = 5
+const GRAVITY: float = 9.8
 
 var direction: Vector3
-var speed: int = 5
-var jump_height: int = 30
+var speed: int = 2
+var jump_vel: int = 4
+var _delta:float = 0.0
+
+var cam_bob
+var obj_cam
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -20,10 +27,30 @@ func _input(event):
 		camera_3d.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
 		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
-func _physics_process(delta) -> void:
-	player_movement()
+
+func _process(delta):
+	cam_movement(delta)
 	
-func player_movement() -> void:
+func cam_movement(delta):
+	_delta += delta
+	
+	if direction != Vector3.ZERO:
+		cam_bob = floor(abs(direction.z) + abs(direction.x)) * _delta * cam_bob_speed
+		obj_cam = origin_cam_pos + Vector3.UP * sin(cam_bob) * cam_bob_up_down	
+	else:
+		cam_bob = floor(abs(1) + abs(1)) * _delta * 0.6
+		obj_cam = origin_cam_pos + Vector3.UP * sin(cam_bob) * 0.1
+		
+	
+	camera_3d.position = camera_3d.position.lerp(obj_cam, delta)
+	
+	
+
+func _physics_process(delta) -> void:
+	process_movement(delta)
+	
+	
+func process_movement(delta) -> void:
 	direction = Vector3.ZERO
 	
 	var h_rot := global_transform.basis.get_euler().y
@@ -36,10 +63,11 @@ func player_movement() -> void:
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
 	
-	if Input.is_action_pressed("action_jump") and is_on_floor():
-		velocity.y += jump_height
 	if !is_on_floor():
-		velocity.y -= GRAVITY
+		velocity.y -= GRAVITY * delta
+	if Input.is_action_just_pressed("action_jump") and is_on_floor():
+		velocity.y = jump_vel
+	
 
 	move_and_slide()
 	
